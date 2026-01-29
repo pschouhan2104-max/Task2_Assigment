5169387b5bf8e9731433a30715d1438cbfff7ede
pschouhan2104-max <pschouhan2104@gmail.com>
2026-01-29 16:51:50 +0530

    ASSIGNMENT 2
    ASSIGNMENT 2
    Module 3: Control Structures in Python
    Task1   Checks whether the number is even or odd using an if-else 
    Task 2   Sum of Integers from 1 to 50 Using a Loop

---
+++ Task1_Ass2.py
@@ -0,0 +1,7 @@
+#WAP of Even or Odd number check
+
+a = int(input("Enter number an integers:"))
+if a%2==0:
+    print("This Nmuber is Even:",a)
+else:
+    print("This Nmuber is Odd:",a)
---
+++ Task2_Ass2.py
@@ -0,0 +1,6 @@
+#WAP of Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+total_sum = 0
+for number in range(1, 51):
+    total_sum += number
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
